e18d272964a329ff0d7d81626d55cbbf1b4bf5d5
DenilsonDesigns <daniellanguilller@gmail.com>
2019-01-28 12:47:02 +1100

    steps done

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,41 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+  //SOLUTION #1
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += "#";
+      } else {
+        stair += " ";
+      }
+    }
+    console.log(stair);
+  }
+}
+
+//Solution with recursion
+function stepsRecursion(n, row = 0, stair = "") {
+  if (n === row) {
+    return;
+  }
+
+  if (n === stair.length) {
+    console.log(stair);
+    return stepsRecursion(n, row + 1);
+  }
+
+  if (stair.length <= row) {
+    stair += "#";
+  } else {
+    stair += " ";
+  }
+  stepsRecursion(n, row, stair);
+}
+
+stepsRecursion(4);
+steps(4);
 
 module.exports = steps;
