448bf32ba62681fe6b559f295252b10c7f8edd80
DenilsonDesigns <daniellanguilller@gmail.com>
2019-01-28 20:27:21 +1100

    finished vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  //MY SOLUTION
+  let count = 0;
+  for (let char of str.toLowerCase()) {
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+  return count;
+
+  //SOLUTION #2
+  //   const matches = str.match(/[aeiou]/gi);
+  //   return matches? matches.length: 0;
+}
+
+vowels("hi there");
 
 module.exports = vowels;
