40066243953273b364601d66702724fbb4820bd9
DenilsonDesigns <daniellanguilller@gmail.com>
2019-02-06 18:15:08 +1100

    finished trees

--- sandbox.js
+++ sandbox.js
@@ -1,83 +1,24 @@
-// Returns a promise that resolves with the average dimensions of all the passed in images
-// Ignores any images that fail to load
-async function loadImages(images) {
-  let dimensions = [];
-  let counter = 0;
-  let widthBucket = 0;
-  let heightBucket = 0;
+function areThereDuplicates() {
+  let obj = {};
+  let duplicate = false;
 
-  for (let i = 0; i < images.length; i++) {
-    await fetch(images[i])
-      .then(res => {
-        console.log(res);
-        if (!res.ok) {
-          console.log("error");
-        }
-        // console.log(res);
-        if (res.ok === true) {
-          dimensions.push(makeObject(res.url));
-          counter++;
-          // console.log(dimensions);
-        } else {
-          console.log("missed me");
-        }
-      })
-      .catch(err => {
-        console.log("Failed to fetch");
-      });
+  for (let i = 0; i < arguments.length; i++) {
+    if (!obj[arguments[i]]) {
+      obj[arguments[i]] = 1;
+    } else {
+      obj[arguments[i]] += 1;
+    }
   }
 
-  for (let i = 0; i < dimensions.length; i++) {
-    widthBucket += dimensions[i].width;
-    heightBucket += dimensions[i].height;
+  for (let key in obj) {
+    if (obj[key] > 1) {
+      duplicate = true;
+    }
   }
-  console.log(widthBucket / counter, heightBucket / counter);
-  return { width: widthBucket / counter, height: heightBucket / counter };
+  console.log(duplicate);
+  return duplicate;
 }
 
-function makeObject(str) {
-  let wSlice = str.indexOf("w=");
-  let hSlice = str.indexOf("h=");
-  let width = Number(str.slice(wSlice + 2, wSlice + 5));
-  let height = Number(str.slice(hSlice + 2, hSlice + 5));
-
-  return { width: width, height: height };
-}
-
-//EXPECTED ANSWER
-const expected = { width: 260, height: 328 };
-
-const images = [
-  {
-    width: 150,
-    height: 150
-  },
-  {
-    width: 350,
-    height: 350
-  },
-  {
-    width: 100,
-    height: 440
-  },
-  {
-    width: 500,
-    height: 200
-  },
-  {
-    width: 200,
-    height: 500
-  }
-];
-
-let imageUrls = images.map(
-  ({ width, height }) =>
-    `https://placeholdit.imgix.net/~text?txtsize=40&txt=${width}x${height}&w=${width}&h=${height}`
-);
-
-imageUrls = imageUrls.concat([
-  "https://not.a.real.domain/bad-image.jpg",
-  "https://fake-site.fake/bad-image.jpg"
-]);
-
-loadImages(imageUrls);
+areThereDuplicates(1, 2, 3); //false
+areThereDuplicates(1, 2, 2); //true
+areThereDuplicates("a", "b", "c", "a"); //true
